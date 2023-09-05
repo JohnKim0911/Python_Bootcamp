030060128f021ca6f49270b7fd40b892e35fb909
JohnKim0911 <john.donggyu.kim@gmail.com>
2023-09-06 00:12:23 +0900

    Review up to Day 02

---
+++ 06_Personal Project/01_Age, Life Calculator/main.py
@@ -0,0 +1,16 @@
+########### Not finished yet. ################
+
+# Object
+# - Calculate age by their birthday.
+# - Calculate remaining days for them.
+
+
+age = input("When is your birthday? ie, :/n")
+#
+#
+# left_years = 90 - int(age)
+# left_months = left_years * 12
+# left_weeks = left_years * 52
+# left_days = left_years * 365
+#
+# print(f"You have {left_days} days, {left_weeks} weeks, and {left_months} months left.")
